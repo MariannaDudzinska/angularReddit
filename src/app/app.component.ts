5ed7f18bc86ea212393ca530f555117fb0069db9
Dudzinska <MDudzinska@ppg.com>
2020-04-02 15:45:47 +0200

    reddit clone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('memeski inne', 'http://hugelol.com', 13),
+      new Article('lista styp', 'http://prs.pwr.edu.pl/?p=5342&fbclid=IwAR0Crf9fFFcCW2TZl-TP1-QJryOlXsGMOXdaOzynsUBSnQU2XWAWN0d8LCQ', 40),
+      new Article('ćmy sornata ', 'https://www.youtube.com/watch?v=RFd6lUXGKfU', 51)
+    ]
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = ''; //clear
+    link.value = ''; //clear
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
